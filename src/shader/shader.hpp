7b85fc67f5babd25cafb1e594c703e68ca7c447f
Gudegi <kang361973@gmail.com>
2024-11-05 17:15:13 +0900

    add docs for doxygen

--- src/shader/shader.hpp
+++ src/shader/shader.hpp
@@ -9,24 +9,69 @@
 
 class Shader{
 
-/*
-    Define the use of one shader program by receiving the vs and fs file paths.
-*/
+///
+/// @brief  The use of one shader program by receiving the vs and fs file paths.
+///
 
 private:
+
+    ///
+    /// @brief Variables conatin shader programs and complied shaders.
+    ///
     GLuint m_shader_program, m_v_shader, m_f_shader;
     const std::string m_vs_path, m_fs_path;
 
+    ///
+    /// @brief Load file from string path. This function is called in "compile" function.
+    /// @param The path of the shader.
+    ///
     std::string loadFile(const std::string path);
+
+    ///
+    /// @brief Compile the shader. This function is called in Shader() function.
+    /// @param The path of the shader and type of shader (ig, GL_VERTEX_SHADER)
+    ///
     GLuint compile(const std::string path, GLenum type); // compile the shader
+
+    ///
+    /// @brief Link the shader and shader program. This function is called in Shader() function.
+    ///
     GLuint link(); // return shader program
+
+
+    ///
+    /// @brief Check compile error.
+    ///
     void checkCompileError(GLuint shader);
+
+    ///
+    /// @brief Check link error.
+    ///
     void checkLinkError(GLuint shader_program);
 
 public:
+
+    ///
+    /// @brief Construct a new shader object.
+    /// @param The paths of vertex and fragment shader.
+    /// @todo Support another shader (ig, geometry shader).
+    ///
     Shader(std::string vs_path, std::string fs_path);
+
+    ///
+    /// @brief Destruct object.
+    ///
     ~Shader();
+
+    ///
+    /// @brief Activate the shader
+    ///
     void use() const;
+
+    ///
+    /// @brief Change shader variables
+    /// @todo Need more types.
+    ///
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
--- src/ui/panel_manager.hpp
+++ src/ui/panel_manager.hpp
@@ -10,21 +10,51 @@
 
 class PanelManager{
 
-/*
-    Define panel manager 
-*/
+///
+/// @brief Define panel manager 
+///
 
 private:
+
+    ///
+    /// @brief list contains the each panels.
+    /// @todo memory leak protection is need.
+    ///
     std::list<Panel*> m_panels;
     //std::list<std::unique_ptr<Panel>> m_panels;
+
+    ///
+    /// @brief Scale imgui contents correspondings to glfw monitor size.
+    ///
     void scaleDPI(GLFWwindow* window);
+
+    ///
+    /// @brief Set imgui theme as dracula theme.
+    ///
     void draculaTheme();
 
 public:
+
+    ///
+    /// @brief Construct the manager that initializes imgui, scales, and, sets theme.
+    ///
     PanelManager(GLFWwindow* window);
+
+    ///
+    /// @brief Destruct panel manager
+    /// @todo check it is ok that when you terminate imgui.
+    /// 
     ~PanelManager();
+
+    ///
+    /// @brief Add a panel to render info. 
+    /// 
     void addPanel(Panel* panel);
     //void addPanel(std::unique_ptr<Panel> panel);
+
+    ///
+    /// @brief Render function. This should be called in main render loop.
+    /// 
     void render();
 
 };
